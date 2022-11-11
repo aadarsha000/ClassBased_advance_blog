70a17d496286576503d3b97c7e0dce2ce5fb1192
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-11-11 09:59:22 +0545

    post form

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "category", "body", "image", "status"]
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control form-control-lg'}),
+            'category': forms.Select(attrs={'class':'form-control form-control-lg'}),
+            'status': forms.Select(attrs={'class':'form-control form-control-lg'}),
+            'body': forms.TextInput(attrs={'class':'form-control form-control-lg'}),
+            'image': forms.FileInput(attrs={'class':'form-control form-control-lg'}),
+        }
