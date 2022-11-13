597447caf2d0d03722773ff985430d6356ccbd83
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-11-13 09:33:55 +0545

    class based

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_create, post_detail, post_update, home
+from .views import post_create, post_detail, post_update
+from .views import HomePostListView
 
 app_name = "blog"
 
 urlpatterns = [
-    path("home/", home, name="home"),
+    path("home/", HomePostListView.as_view(), name="home"),
     path("create/", post_create, name="post_create"),
     path("detail/<int:id>/<slug:slug>/<int:category>/", post_detail, name="post_detail"),
     path("update/", post_update, name="post_update"),
--- blog/views.py
+++ blog/views.py
@@ -4,31 +4,46 @@ from .forms import PostForm, CommentForm
 from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-
+from django.views.generic import ListView, DetailView
 
 
 # fuction based view
 
 #home view that show list of post
-def home(request):
-    posts = Post.objects.all().filter(status="PUBLISHED")
-    category = Category.objects.all()
-    paginator = Paginator(posts, 2)
-    page_number = request.GET.get('page', 1)
-    try:
-        page_obj = paginator.page(page_number)
-    except PageNotAnInteger:
-        # if page is not an integer, deliver the first page
-        page_obj = paginator.page(1)
-    except EmptyPage:
-        # if the page is out of range, deliver the last page
-        page_obj = paginator.page(paginator.num_pages)
-    context = {
-        "posts":posts,
-        "category":category,
-        "page_obj":page_obj,
-    }
-    return render(request, "blog/index.html", context)
+# def home(request):
+#     posts = Post.objects.all().filter(status="PUBLISHED")
+#     category = Category.objects.all()
+#     paginator = Paginator(posts, 2)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         page_obj = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # if page is not an integer, deliver the first page
+#         page_obj = paginator.page(1)
+#     except EmptyPage:
+#         # if the page is out of range, deliver the last page
+#         page_obj = paginator.page(paginator.num_pages)
+#     context = {
+#         "posts":posts,
+#         "category":category,
+#         "page_obj":page_obj,
+#     }
+#     return render(request, "blog/index.html", context)
+
+# home view using class
+class HomePostListView(ListView):
+    model = Post
+    paginate_by = 2
+    context_object_name = "posts"
+    template_name = "blog/index.html"
+
+    def get_queryset(self):
+        return Post.objects.all().filter(status="PUBLISHED")
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
+        return context
 
 
 
@@ -62,6 +77,32 @@ def post_detail(request, id, slug, category):
     context["posts"]=posts
     return render(request, "blog/postdetail.html", context)
 
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/postdetail.html"
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        print("hello")
+        if form.is_valid():
+            new_comment=form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post
+            print("hi")
+            new_comment.save()
+            print("hello")
+        else:
+            print("error")
+    else:
+        form = CommentForm()
+        print("else")
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"]= get_object_or_404(Post, id=id, status="PUBLISHED")
 
 
 # Create the post by only login user
